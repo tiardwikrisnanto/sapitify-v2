ac911306365500937e17524e7f3a4f27f1df5a88
tiardwikrisnanto <44857347+tiardwikrisnanto@users.noreply.github.com>
2022-03-23 15:21:49 +0700

    HW module 2 React Component

---
+++ src/component/Conten/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import '../../styles/index.css';
+import data from '../../data/dataSpotify';
+
+export const Conten = () =>  {
+    return (
+        <div className = "conten">
+        <div>
+            <img src={data.album.images[0].url} className="conten_image" />
+        </div>
+
+        <div>
+            <h2>{data.name}</h2>
+            <h3>{data.album.name}</h3>
+            <p>{data.artists[0].name}</p>
+            <button><a href={data.album.artists[0].external_urls.spotify}>Select</a></button>
+        </div>
+        </div>
+    )
+}
---
+++ src/home/Home.js
@@ -0,0 +1,10 @@
+import React from "react";
+import {Conten} from "../component/Conten";
+
+export default function Home() {
+    return (
+        <div className="main-conten">
+            <Conten />
+        </div>
+    )
+}
