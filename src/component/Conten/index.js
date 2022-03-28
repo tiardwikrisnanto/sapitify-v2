acedf4326427b7e966b2d47d852ad60f37f588ee
tiardwikrisnanto <44857347+tiardwikrisnanto@users.noreply.github.com>
2022-03-28 14:54:51 +0700

    looping conten HW

--- src/component/Conten/index.js
+++ src/component/Conten/index.js
@@ -2,21 +2,26 @@ import React from 'react';
 import '../../styles/index.css';
 import data from '../../data/dataSpotify';
 
-export const Conten = () =>  {
+export const Conten = () => {
 
     return (
-        <div className = "conten">
-        <div>
-            <img src={data.album.images[0].url} className="conten_image" />
-        </div>
+        <>
+            {data.map((song) => {
+                return (<div className="conten">
+                    <div>
+                        <img src={song.album.images[0].url} className="conten_image" />
+                    </div>
 
-        <div>
-            {/* Sudah sesuai ditaruh diluar main component sejak minggu lalu */}
-            <h2>{data.name}</h2>
-            <h3>{data.album.name}</h3>
-            <p>{data.artists[0].name}</p>
-            <button><a href={data.album.artists[0].external_urls.spotify}>Select</a></button>
-        </div>
-        </div>
+                    <div>
+                        {/* Sudah sesuai ditaruh diluar main component sejak minggu lalu */}
+                        <h2>{song.name}</h2>
+                        <h3>{song.album.name}</h3>
+                        <p>{song.artists[0].name}</p>
+                        <button><a href={song.album.artists[0].external_urls.spotify}>Select</a></button>
+                    </div>
+                </div>)
+            })}
+        </>
+
     )
 }
