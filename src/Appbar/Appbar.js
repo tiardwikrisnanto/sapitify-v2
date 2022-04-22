43043696f2dbd3a354233d4c318d06b77f3ecbcf
tiardwikrisnanto <44857347+tiardwikrisnanto@users.noreply.github.com>
2022-04-22 22:41:41 +0700

    update image

--- src/Appbar/Appbar.js
+++ src/Appbar/Appbar.js
@@ -6,10 +6,8 @@ import Toolbar from '@mui/material/Toolbar';
 import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
 import InputBase from '@mui/material/InputBase';
-
 import MenuItem from '@mui/material/MenuItem';
 import Menu from '@mui/material/Menu';
-
 import SearchIcon from '@mui/icons-material/Search';
 import Avatar from '@mui/material/Avatar';
 import {useNavigate} from 'react-router-dom';
